1cb52f24b893ec2f7b28fc3f1158f00ab5960c5f
Leon <zhouliang19910719@gmail.com>
2020-04-27 15:50:48 +1200

    updated shop data, update utils for cart reducer

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,13 @@
+
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+    const isExist = cartItems.find(item => item.id === cartItemToAdd.id);
+
+    if (isExist) {
+        return cartItems.map(item =>
+            item.id === cartItemToAdd.id
+                ? { ...item, quantity: item.quantity + 1 } : item
+        );
+    }
+
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
+}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(...middlewares)));
 
 export default store;
