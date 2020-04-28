e0daad18b214758fc816647a4d85c3fd25d82abe
Leon <zhouliang19910719@gmail.com>
2020-04-28 17:38:46 +1200

    add stripe for checkout

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selector';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe/stripe-button.component';
 
 
 const Checkout = ({ cartItems, total }) => (
@@ -16,9 +17,13 @@ const Checkout = ({ cartItems, total }) => (
             <div className="header-block"><span>Remove</span></div>
         </div>
         {cartItems.map(item => <CheckoutItem key={item.id} cartItem={item} />)}
-        <div className="total">
-            <span>Total: ${total}</span>
+        <div className="total">Total: ${total}</div>
+        <div className='test-warning'>
+            *Please use the following test credit card for payments*
+            <br />
+            4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
         </div>
+        <StripeCheckoutButton price={total} />
     </div>
 );
 const mapStateToProps = createStructuredSelector({
