c62b4f5cf721dafc417c52be3814b4b9cd35be0f
Leon <zhouliang19910719@gmail.com>
2020-05-04 14:39:44 +1200

    fix

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-const compression = require('compression');
-const enforce = require('express-sslify');
+// const compression = require('compression');
+// const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
@@ -19,8 +19,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
-    app.use(compression);
-    app.use(enforce.HTTPS({ trustProtoHeader: true }));
+    // app.use(compression);
+    // app.use(enforce.HTTPS({ trustProtoHeader: true }));
     app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', function(req, res){
