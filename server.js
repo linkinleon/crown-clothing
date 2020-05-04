97edc743addff40e493f679d321185f75b05e8a8
Leon <zhouliang19910719@gmail.com>
2020-05-04 14:23:37 +1200

    fix service worker

--- server.js
+++ server.js
@@ -35,9 +35,9 @@ app.listen(port, error => {
     console.log('server running on port: ' + port);
 })
 
-app.get("/service-worker.js", (req, res)=>{
-    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
-})
+// app.get("/service-worker.js", (req, res)=>{
+//     res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+// })
 
 app.post('/payment', (req, res) => {
     const body = {
